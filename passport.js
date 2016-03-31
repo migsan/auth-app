e81c17b49aaf908e6a06b6c445115ca00035db27
Miguel Santamaria <santamaria.migue@gmail.com>
2016-03-31 11:58:47 -0500

    Test

--- passport.js
+++ passport.js
@@ -61,27 +61,29 @@ module.exports = function(passport) {
 		callbackURL: '/auth/google/callback'
 	}, function(token, tokenSecret, profile, done) {
 		User.findOne({ googleId: profile.id }, function(err, user) {
-			console.log('--------- the user is', user);
+			console.log(user);
 
 			if ( err ) {
 				throw(err);
 			}
 			if ( !err && user!=null ) {
-				if ( profile.id != user.provider_id ) {
-					var user = new User({
-						provider_id: profile.id,
-						provider: profile.provider,
-						name: profile.displayName,
-						photo: profile.photos[0].value
-					});
-					user.save(function(err) {
-						if ( err ) {
-							throw(err);
-						}
-						return done(null, user);
-					});
-				}
+				return done(null, user);
 			}
+
+			var user = new User({
+				provider_id: profile.id,
+				provider: profile.provider,
+				name: profile.displayName,
+				photo: profile.photos[0].value
+			});
+
+			user.save(function(err) {
+				if ( err ) {
+					throw(err);
+				}
+
+				done(null, user);
+			});
 		});
 	}));
 
