7ab59876df023ef4a472ea9e8117cdf2edd80378
Miguel Santamaria <santamaria.migue@gmail.com>
2016-03-31 09:03:52 -0500

    Adding comments on index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,8 @@
 // Rutas de la aplicación
 
 exports.index = function(req, res){
-  // Renderiza la plantilla 'index' cuando en el navegador
-  // nos encontremos en la raiz '/' --> http://localhost:puerto/
   res.render('index', {
-    // Enviamos como variables un título
-    // y objeto 'user' que contiene toda
-    // la información del usuario y viaja en el 'request'
+    // sending user information and jade locals
     title: 'Ejemplo de Passport JS',
     user: req.user
   });
